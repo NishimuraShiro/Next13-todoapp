3ae5a1955d0eaf723fd85ecdd31be9c1ac301961
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-09-04 23:33:32 +0900

    フロント編集機能

--- todo-next13/src/app/components/Todo.tsx
+++ todo-next13/src/app/components/Todo.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Task } from "../types/tasks";
 
 interface TaskProps {
@@ -6,11 +8,38 @@ interface TaskProps {
 }
 
 const Todo = ({ task }: TaskProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskTitle, setEditedTaskTitle] = useState(task.text);
+  const handleEdit = async () => {
+    setIsEditing(true);
+  };
+  const handleSave = async () => {
+    setIsEditing(false);
+  };
   return (
     <li key={task.id} className="flex justify-between p-4">
-      <span>{task.text}</span>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedTaskTitle}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEditedTaskTitle(e.target.value)
+          }
+        />
+      ) : (
+        <span>{task.text}</span>
+      )}
       <div>
-        <button className="px-2">編集</button>
+        {isEditing ? (
+          <button className="px-2" onClick={handleSave}>
+            保存
+          </button>
+        ) : (
+          <button className="px-2" onClick={handleEdit}>
+            編集
+          </button>
+        )}
+
         <button className="px-2">削除</button>
       </div>
     </li>
