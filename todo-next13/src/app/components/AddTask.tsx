f14c5a05f73f617b1ab7224739bd0ce311cbaa52
HA-mi-lton <hamiltonshiro3@gmail.com>
2023-09-08 22:58:07 +0900

    空欄の際にエラーを出力させる実装

--- todo-next13/src/app/components/AddTask.tsx
+++ todo-next13/src/app/components/AddTask.tsx
@@ -6,10 +6,17 @@ import { v4 as uuidv4 } from "uuid";
 
 const AddTask = () => {
   const [taskTitle, setTaskTitle] = useState("");
+  const [empty, setEmpty] = useState(false);
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    await addTodo({ id: uuidv4(), text: taskTitle });
-    setTaskTitle("");
+    if (taskTitle.trim() === "") {
+      setEmpty(true);
+    } else {
+      await addTodo({ id: uuidv4(), text: taskTitle });
+      setTaskTitle("");
+      setEmpty(false);
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
@@ -21,6 +28,7 @@ const AddTask = () => {
         }
         value={taskTitle}
       />
+      {empty && <p className="text-red-500">入力欄が空欄です。</p>}
       <button className="w-full px-4 py-2">Add task</button>
     </form>
   );
